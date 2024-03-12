fb499608d7cb862516147d3abd43661295646360
Egor Volobuev <63512848+Siberian-Joe@users.noreply.github.com>
2024-03-13 05:48:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+
 # Installing modules dependencies
-echo "Installing sequelize-cli globally"
-npm install -g sequelize-cli
+echo Installing sequelize-cli globally 
+sudo npm install -g sequelize-cli
 
-echo "Module installation has been initiated"
-npm install
+echo Module intallation has been initiated
+sudo npm install
 
-echo "Install pm2 globally"
-npm i -g pm2
+echo Install pm2 globally
+sudo npm i -g pm2
 
-echo "Start node server using pm2"
-pm2 start app.js --name server
+echo Start node server using pm2
+sudo pm2 start app.js --name server
